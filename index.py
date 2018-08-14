e8d398df8f94c8b0b8f5a1cea9149aec74ef5559
junaidfiaz143@gmail.com <junaidfiaz143@gmail.com>
2018-08-14 22:41:16 +0500

    working todo flask python app

--- index.py
+++ index.py
@@ -1,24 +1,64 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+import sqlite3
+
+def create_table():
+	c.execute("CREATE TABLE IF NOT EXISTS todo_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, status TEXT)")
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('add_todo_form.html')
+
+	conn = sqlite3.connect('todo.db')
+	c = conn.cursor()
+
+	c.execute('SELECT * FROM todo_tasks')
+	todos = c.fetchall()
+
+	return render_template('index.html', todos = todos)
 
 @app.route('/process', methods=['POST'])
 def process():
+
     # title = request.form.get('titleInput')
     # description = request.form.get('descriptionInput')
     # return ("<b style='font-family: Kristen ITC;'>Title: " + title + "  Description: " + description + "<b>")
     # return render_template("display.html", title = title)
-    title= request.form['title']
-    # descriptionInput = request.form['description']
+	title = request.form['title']
+	i = 0
 
-    if title:
-    	return jsonify({'title' : title})
+	conn = sqlite3.connect('todo.db')
+	c = conn.cursor()
 
-    return jsonify({'error': "Missing data!"})
+	def get_last_id():
+		c.execute("SELECT * FROM todo_tasks")
+		result_set = c.fetchall()
+		for row in result_set:
+		    i = row[0]
+
+		print("Latest id: ", i)
+		conn.commit()
+
+		return i
+
+	def enter_data(paramTitle):
+		i = get_last_id() + 1
+
+		c.execute('INSERT INTO todo_tasks VALUES(' + str(i) + ', "' + paramTitle + '", 1)')
+		conn.commit()
+
+		c.execute('SELECT * FROM todo_tasks')
+		todos = c.fetchall()
+
+		return render_template('index.html', todos = todos)
+
+		# return redirect(url_for('index'))
+		# return jsonify({'title' : 'TODO task added!'})
+
+	if title:
+		enter_data(title)
+	else:
+		return jsonify({'error': "Missing data!"})
 
 
 if __name__ == '__main__':
